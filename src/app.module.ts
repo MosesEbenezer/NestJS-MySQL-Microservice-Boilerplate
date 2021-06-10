a6309b1fcb03504e85e9686f961314f95311377c
Moses Ebenezer <moseschimdike@gmail.com>
2021-06-10 11:14:22 +0100

    initial db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from './admin/admin.module';
 import { MerchantAdminModule } from './merchant-admin/merchant-admin.module';
 import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [AdminModule, MerchantAdminModule, PaymentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AdminModule,
+    MerchantAdminModule,
+    PaymentModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: false,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
